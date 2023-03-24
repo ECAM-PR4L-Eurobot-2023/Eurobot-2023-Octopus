565404572867c83e8ed0a936476ca20c120266c8
fred-corp <7109145+fred-corp@users.noreply.github.com>
2023-03-24 13:47:09 +0100

    made test sequence

--- src/test/test.ino
+++ src/test/test.ino
@@ -1,12 +1,26 @@
+#include <ESP32Servo.h>
+
 #define FAN 23
+#define TRAP_LEFT 25
+#define TRAP_RIGHT 33
+
+Servo trapLeft;
+Servo trapRight;
 
 void setup() {
   pinMode(FAN, OUTPUT);
+  trapLeft.attach(TRAP_LEFT);
+  trapRight.attach(TRAP_RIGHT);
 }
 
 void loop() {
+  trapLeft.write(0);
+  delay(1000);
+  trapLeft.write(95);
+  delay(200);
   digitalWrite(FAN, HIGH);
-  delay(5000);
+  delay(10000);
   digitalWrite(FAN, LOW);
-  delay(5000);
+  trapLeft.write(0);
+  while(1);
 }
